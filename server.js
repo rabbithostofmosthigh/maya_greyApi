ac51f8fa6ddfe4f1669015ed364bde4f2cdedc65
Ikechukuwuprince <Ikechukuwuprince@gmail.com>
2025-05-09 18:02:12 +0100

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000; // port to connect to WEB
 // emails credentials
 const userEmail = "Ikechukuwuprince@gmail.com";
 const pass = "hznikzfudykeryam";
-// 19 may
+// 9th June
 
 // Middleware
 app.use(express.json());
