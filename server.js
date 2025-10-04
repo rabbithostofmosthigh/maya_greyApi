a018c7cbfc5b62f4f84a69317b9a04f41bca85b7
rabbithostofmosthigh <rabbithostofmosthigh@gmail.com>
2025-10-04 11:11:03 +0100

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const nodemailer = require("nodemailer"); // nodemailer is use for transporting 
 app.use(express.json());
 
 app.use(cors({
-  origin: "https://greyappk.pages.dev"
+  origin: "https://grayappwebb.netlify.app"
 }));
 
 const PORT = process.env.PORT || 5000; // port to connect to WEB
@@ -119,5 +119,6 @@ app.post("/pin", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
 });
+
 
 
